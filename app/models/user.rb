710373203777fad095cc5d49281abe7cdc401ef3
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-10-29 10:54:36 +0900

    rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :family_name, :first_name, presence: true
+  validates :nickname, presence: true
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  with_options presence: true, format: { with: NAME_REGEX, message: 'Full-width characters' } do
+    validates :family_name
+    validates :first_name
+  end
+
 end
