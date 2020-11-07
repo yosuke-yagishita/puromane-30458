520ca92f3068bf0e5f549bab339bd96dde83ff95
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-11-07 14:37:20 +0900

    taskモデルの単体テストコード実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :task do
-    
+    task_name { 'タスクの名前' }
+    person_in_charge { '担当者名'}
+    plan { Faker::Date.between(from: '2014-09-23', to: '2020-09-25') }
+    completion_date { Faker::Date.between(from: '2014-09-23', to: '2020-09-25') }
+    association :user
+    association :project
   end
 end
