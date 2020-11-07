fc2e7263dcfcc4a8f658a4d712e06f06ff804c5e
yagishi-tan <ochiteyuku@hotmail.co.jp>
2020-11-07 20:07:09 +0900

    タスクの新規作成エラー時のrender修正

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,9 +52,7 @@ class ProjectsController < ApplicationController
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
@@ -75,9 +73,7 @@ class ProjectsController < ApplicationController
         end
       end
 
-      # wday_numの定義
       wday_num = Date.today.wday + x
-      # wday_numの数値が７以上になった場合の条件分岐
       if wday_num >= 7 then
         wday_num = wday_num - 7
       end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
 
 def create
+  get_week
   @task = Task.new(task_params)
+  @tasks = @project.tasks
+  @project = @task.project
   if @task.save
     redirect_to project_path(@task.project)
   else
-    redirect_to project_path(@task.project)
+    render "projects/show"
   end
 end
 
@@ -34,5 +37,38 @@ end
 
   def task_params
     params.require(:task).permit(:task_name, :person_in_charge, :plan, :completion_date).merge(user_id: current_user.id, project_id: params[:project_id])
+  end
+
+  def get_week
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+    @todays_date = Date.today
+
+    @week_days = []
+
+    @project = Project.find(params[:project_id])
+    tasks = @project.tasks
+
+    7.times do |x|
+      today_plans = []
+      task = tasks.map do |task|
+        if (task.plan == @todays_date + x) && (task.completion_date == @todays_date + x)
+          today_plans.push('★')
+        elsif task.plan == @todays_date + x
+          today_plans.push('○')
+        elsif task.completion_date == @todays_date + x
+          today_plans.push('●')
+        else
+          today_plans.push('ー')
+        end
+      end
+
+      wday_num = Date.today.wday + x
+      if wday_num >= 7 then
+        wday_num = wday_num - 7
+      end
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+      @week_days.push(days)
+    end
   end
 end
